eabdce9e4507472aaf8582a48964dfdfb1b7ee4f
Witchaya <witchaya.ungp@bumail.net>
2021-10-08 20:45:13 +0700

    Progress Update
    Death Trigger & Jump

---
+++ FappyNok/Assets/CollisionTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Dead");
+        }
+    }
+}
