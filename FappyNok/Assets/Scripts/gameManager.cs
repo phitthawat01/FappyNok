3c71b31bf26af98a7b163b9702bc95c90286b889
Witchaya <witchaya.ungp@bumail.net>
2021-10-08 18:53:16 +0700

    Delete Script
    Deleted gameManager and SpriteConstantMove scripts

--- FappyNok/Assets/Scripts/gameManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class gameManager : MonoBehaviour
-{
-    public float moveSpeed = 1.0f;
-    public Vector2 moveVector;
-}
